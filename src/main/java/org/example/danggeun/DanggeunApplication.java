3061ee459901cfb1777f47da5fe2666af5059604
Jaekyung89 <kjkstone08@naver.com>
2025-07-03 11:16:24 +0900

    Initial commit

---
+++ src/main/java/org/example/danggeun/DanggeunApplication.java
@@ -0,0 +1,13 @@
+package org.example.danggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanggeunApplication.class, args);
+	}
+
+}
