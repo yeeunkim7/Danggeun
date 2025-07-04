7c9057cce869c01df8343fc09b1f58ed4c040f0b
BlueGraffe-hub <griyeen@gmail.com>
2025-07-04 16:31:21 +0900

    Main html_css파일통합0704PM

---
+++ src/main/java/org/example/danggeun/controller/mainPageController.java
@@ -0,0 +1,12 @@
+package org.example.danggeun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainPageController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
