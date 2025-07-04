e6466ec8ffed19d23a46a8ca2adb79c12e5cabf7
김민형 <132879692+KimTeaHyeong1@users.noreply.github.com>
2025-07-04 11:39:42 +0900

    delete Security

---
+++ src/main/java/org/example/danggeun/repository/WriteRepository.java
@@ -0,0 +1,7 @@
+package org.example.danggeun.repository;
+
+import org.example.danggeun.domain.Write;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WriteRepository extends JpaRepository<Write, Long> {
+}
