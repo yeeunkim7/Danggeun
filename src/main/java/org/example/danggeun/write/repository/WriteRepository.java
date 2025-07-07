fd0f79c0f8dcc09407d67ae9d27186b4310cebdc
김민형 <132879692+KimTeaHyeong1@users.noreply.github.com>
2025-07-07 14:10:58 +0900

    WritePage 와 TradePost Page 를 합쳐서 커밋 했습니다.

--- src/main/java/org/example/danggeun/write/repository/WriteRepository.java
+++ src/main/java/org/example/danggeun/write/repository/WriteRepository.java
@@ -1,6 +1,6 @@
 package org.example.danggeun.write.repository;
 
-import org.example.danggeun.domain.Write;
+import org.example.danggeun.write.entity.Write;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WriteRepository extends JpaRepository<Write, Long> {
